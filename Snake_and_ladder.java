33607a04344bec5c887885f09db32540410c3f4a
Ashok Mane <ashokmane81800@gmail.com>
2021-04-06 12:22:27 +0530

    [ashok] created a start position of a plyer

---
+++ Snake_and_ladder.java
@@ -0,0 +1,14 @@
+
+public class Snake_and_ladder {
+	public static void main(String[] args) {
+		System.out.println("****WELCOME TO SNAKE AND LADDER PROBLEM *****");
+		int player1 = 0;
+		int position = 0;
+		System.out.println();
+		System.out.println("player start its position from " +position + "player1");
+		
+		
+		
+	}
+
+}
